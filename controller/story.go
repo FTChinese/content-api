8f142f7a64f21e13d748f8ef9fba697f09a7e656
weiguo.ni <neefrankie@gmail.com>
2019-09-09 16:58:22 +0800

    feat: story routers

--- controller/story.go
+++ controller/story.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"github.com/FTChinese/go-rest/view"
 	"github.com/jmoiron/sqlx"
+	"gitlab.com/ftchinese/content-api/models"
 	"gitlab.com/ftchinese/content-api/repository"
 	"net/http"
 )
@@ -34,13 +35,61 @@ func (router StoryRouter) Raw(w http.ResponseWriter, req *http.Request) {
 }
 
 func (router StoryRouter) CN(w http.ResponseWriter, req *http.Request) {
+	id, err := GetURLParam(req, "id").ToString()
+	if err != nil {
+		_ = view.Render(w, view.NewBadRequest(err.Error()))
+		return
+	}
 
+	rawStory, err := router.env.RetrieveRawStory(id)
+	if err != nil {
+		_ = view.Render(w, view.NewDBFailure(err))
+		return
+	}
+
+	_ = view.Render(w, view.NewResponse().SetBody(models.NewStoryCN(rawStory)))
 }
 
 func (router StoryRouter) EN(w http.ResponseWriter, req *http.Request) {
+	id, err := GetURLParam(req, "id").ToString()
+	if err != nil {
+		_ = view.Render(w, view.NewBadRequest(err.Error()))
+		return
+	}
 
+	rawStory, err := router.env.RetrieveRawStory(id)
+	if err != nil {
+		_ = view.Render(w, view.NewDBFailure(err))
+		return
+	}
+
+	s, err := models.NewStoryEN(rawStory)
+	if err != nil {
+		_ = view.Render(w, view.NewNotFound())
+		return
+	}
+
+	_ = view.Render(w, view.NewResponse().SetBody(s))
 }
 
 func (router StoryRouter) Bilingual(w http.ResponseWriter, req *http.Request) {
+	id, err := GetURLParam(req, "id").ToString()
+	if err != nil {
+		_ = view.Render(w, view.NewBadRequest(err.Error()))
+		return
+	}
 
+	rawStory, err := router.env.RetrieveRawStory(id)
+	if err != nil {
+		_ = view.Render(w, view.NewDBFailure(err))
+		return
+	}
+
+	s, err := models.NewBilingualStory(rawStory)
+	if err != nil {
+		_ = view.Render(w, view.NewNotFound())
+		return
+	}
+
+	_ = view.Render(w, view.NewResponse().SetBody(s))
 }
