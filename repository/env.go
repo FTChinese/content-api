815ecec50db1cbbdd074cc711a59858148c81118
weiguo.ni <neefrankie@gmail.com>
2019-10-14 17:15:47 +0800

    feat: add go-cache

--- repository/env.go
+++ repository/env.go
@@ -2,15 +2,22 @@ package repository
 
 import (
 	"github.com/jmoiron/sqlx"
+	"github.com/patrickmn/go-cache"
 	"github.com/sirupsen/logrus"
+	"time"
 )
 
 type Env struct {
-	db *sqlx.DB
+	db    *sqlx.DB
+	cache *cache.Cache
 }
 
 func NewEnv(db *sqlx.DB) Env {
-	return Env{db: db}
+	return Env{
+		db: db,
+		// Default expiration 24 hours, and purges the expired items every hour.
+		cache: cache.New(24*time.Hour, 1*time.Hour),
+	}
 }
 
 var logger = logrus.WithField("package", "content-api.repository")
