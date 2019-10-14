70f3fa713d32fc237dc3760b09162aade1d8b1af
weiguo.ni <neefrankie@gmail.com>
2019-10-14 11:23:07 +0800

    feat: video and gallery controller

--- controller/gallery.go
+++ controller/gallery.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"github.com/FTChinese/go-rest/view"
 	"github.com/jmoiron/sqlx"
 	"gitlab.com/ftchinese/content-api/repository"
 	"net/http"
@@ -15,5 +16,19 @@ func NewGalleryStory(db *sqlx.DB) GalleryRouter {
 }
 
 func (router GalleryRouter) Article(w http.ResponseWriter, req *http.Request) {
+	id, err := GetURLParam(req, "id").ToInt()
 
+	if err != nil {
+		_ = view.Render(w, view.NewBadRequest(err.Error()))
+		return
+	}
+
+	g, err := router.env.RetrieveGallery(id)
+
+	if err != nil {
+		_ = view.Render(w, view.NewDBFailure(err))
+		return
+	}
+
+	_ = view.Render(w, view.NewResponse().SetBody(g))
 }
