febdf897a40d3ca22c968efb67e2171536692860
weiguo.ni <neefrankie@gmail.com>
2019-10-14 11:06:20 +0800

    feat: retrieve video and gallery

---
+++ repository/galleries.go
@@ -0,0 +1,74 @@
+package repository
+
+import "gitlab.com/ftchinese/content-api/models"
+
+type imageResult struct {
+	success []models.GalleryItem
+	err     error
+}
+
+type galleryResult struct {
+	success models.Gallery
+	err     error
+}
+
+func (env Env) RetrieveGalleryImages(id int64) ([]models.GalleryItem, error) {
+	var items = []models.GalleryItem{}
+
+	if err := env.db.Select(&items, stmtGalleryImages, id); err != nil {
+		return items, err
+	}
+
+	return items, nil
+}
+
+func (env Env) RetrieveGalleryBody(id int64) (models.Gallery, error) {
+	var gallery models.Gallery
+
+	if err := env.db.Get(&gallery, stmtGallery, id); err != nil {
+		return gallery, err
+	}
+
+	return gallery, nil
+}
+
+func (env Env) RetrieveGallery(id int64) (models.Gallery, error) {
+	imageChan := make(chan imageResult)
+	bodyChan := make(chan galleryResult)
+
+	go func() {
+		images, err := env.RetrieveGalleryImages(id)
+		imageChan <- imageResult{
+			success: images,
+			err:     err,
+		}
+	}()
+
+	go func() {
+		body, err := env.RetrieveGalleryBody(id)
+		bodyChan <- galleryResult{
+			success: body,
+			err:     err,
+		}
+	}()
+
+	iResult, gResult := <-imageChan, <-bodyChan
+
+	if iResult.err != nil {
+		return models.Gallery{}, iResult.err
+	}
+
+	if gResult.err != nil {
+		return models.Gallery{}, gResult.err
+	}
+
+	for _, item := range iResult.success {
+		item.Normalize()
+	}
+
+	gResult.success.Normalize()
+
+	gResult.success.Items = iResult.success
+
+	return gResult.success, nil
+}
