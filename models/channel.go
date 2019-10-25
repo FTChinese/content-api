7df153e7c24bf133e6a219e88d617f7cb81cf14c
weiguo.ni <neefrankie@gmail.com>
2019-10-25 15:01:17 +0800

    feat: channel page and navigation data types

--- models/channel.go
+++ models/channel.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"github.com/FTChinese/go-rest/chrono"
+	"github.com/guregu/null"
 	"strings"
 )
 
@@ -30,6 +31,22 @@ type ArchivedFrontPage struct {
 	Data []Teaser `json:"data"`
 }
 
+// ChannelSetting represents one row in the channel table.
+// This controls the configuration of a channel.
+type ChannelSetting struct {
+	ID          int64       `json:"id" db:"id"`
+	ParentID    int64       `json:"parentId" db:"parent_id"`
+	FullPath    string      `json:"fullPath" db:"full_path"`
+	Name        string      `json:"name"`
+	Title       string      `json:"title" db:"title"`
+	Description null.String `json:"description" db:"description"`
+	CreatedAt   chrono.Time `json:"createdAt" db:"created_utc"`
+	UpdatedAt   chrono.Time `json:"updatedAt" db:"updated_utc"`
+}
+
+// ChannelPage contains all the data a channel required, including
+// its configuration and a list of article summaries.
 type ChannelPage struct {
+	ChannelSetting
 	Data []Teaser `json:"data"`
 }
