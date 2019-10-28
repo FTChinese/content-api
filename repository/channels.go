fb6685647be98f972b364f0d518e76811ab93df5
weiguo.ni <neefrankie@gmail.com>
2019-10-28 17:48:14 +0800

    refactor: move RetrieveTeasers

--- repository/channels.go
+++ repository/channels.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	gorest "github.com/FTChinese/go-rest"
 	"github.com/FTChinese/go-rest/chrono"
 	"github.com/jmoiron/sqlx"
 	"github.com/patrickmn/go-cache"
@@ -74,6 +75,8 @@ func (env ChannelEnv) ListChannels() ([]models.ChannelSetting, error) {
 	var data = make([]models.ChannelSetting, 0)
 
 	if err := env.db.Select(&data, stmtListChannels); err != nil {
+		logger.WithField("trace", "ChannelEnv.ListChannels")
+
 		return data, err
 	}
 
@@ -161,4 +164,82 @@ func (env ChannelEnv) retrieveChannel(pathName string) (models.ChannelSetting, e
 
 func (env ChannelEnv) cacheChannel(name string, c models.ChannelSetting) {
 	env.cache.Set(prefixChannelConfig+name, c, cache.DefaultExpiration)
+}
+
+type chConfigResult struct {
+	success models.ChannelSetting
+	err     error
+}
+
+type teasersResult struct {
+	success []models.Teaser
+	err     error
+}
+
+// RetrieveTeasers selects all article summary belong to a
+// channel.
+func (env ChannelEnv) RetrieveTeasers(channelID int64, p gorest.Pagination) ([]models.Teaser, error) {
+	var t = make([]models.Teaser, 0)
+
+	err := env.db.Select(&t, stmtChannelContent,
+		channelID, p.Limit, p.Offset(),
+	)
+	if err != nil {
+		return t, err
+	}
+
+	return t, err
+}
+
+func (env ChannelEnv) LoadPage(channelName string, p gorest.Pagination) (models.ChannelPage, error) {
+	log := logger.WithField("trace", "ChannelEnv.LoadPage")
+
+	chMap, err := env.LoadChannelMap()
+	if err != nil {
+		log.Error(err)
+		return models.ChannelPage{}, err
+	}
+
+	chID, ok := chMap[channelName]
+	if !ok {
+		return models.ChannelPage{}, err
+	}
+
+	configChan := make(chan chConfigResult)
+	teasersChan := make(chan teasersResult)
+
+	go func() {
+		chConfig, err := env.LoadChannel(channelName)
+
+		configChan <- chConfigResult{
+			success: chConfig,
+			err:     err,
+		}
+	}()
+
+	go func() {
+		teasers, err := env.RetrieveTeasers(chID, p)
+
+		teasersChan <- teasersResult{
+			success: teasers,
+			err:     err,
+		}
+	}()
+
+	cResult, tResult := <-configChan, <-teasersChan
+
+	if cResult.err != nil {
+		log.Error(cResult.err)
+		return models.ChannelPage{}, cResult.err
+	}
+
+	if tResult.err != nil {
+		log.Error(tResult.err)
+		return models.ChannelPage{}, tResult.err
+	}
+
+	return models.ChannelPage{
+		ChannelSetting: cResult.success,
+		Data:           tResult.success,
+	}, nil
 }
