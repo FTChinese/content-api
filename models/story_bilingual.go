aade6ab5b733122fbd620f90bdc66f1e4520d31a
weiguo.ni <neefrankie@gmail.com>
2020-01-22 15:28:37 +0800

    refactor: drop NewBilingualStory

--- models/story_bilingual.go
+++ models/story_bilingual.go
@@ -12,32 +12,3 @@ type BilingualStory struct {
 	AlternativeTitles AlternativeTitles `json:"alternativeTitles"`
 	Related           []ArticleMeta     `json:"related"`
 }
-
-func NewBilingualStory(raw *RawStory) BilingualStory {
-
-	s := BilingualStory{
-		Teaser:     raw.Teaser(),
-		StoryBase:  raw.StoryBase(),
-		Body:       []Bilingual{},
-		Translator: null.String{},
-		AlternativeTitles: AlternativeTitles{
-			English:   null.StringFrom(raw.TitleEN),
-			Promotion: null.String{},
-		},
-		Related: raw.Related,
-	}
-
-	cnParas, translator := raw.splitCNWithTranslator()
-	s.Translator = null.NewString(translator, translator != "")
-
-	pairs := ZipString(cnParas, raw.splitEN())
-
-	for _, v := range pairs {
-		s.Body = append(s.Body, Bilingual{
-			CN: v.First,
-			EN: v.Second,
-		})
-	}
-
-	return s
-}
