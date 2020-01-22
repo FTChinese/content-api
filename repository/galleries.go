cff0ef97663cee30874822054fc063866761aab9
weiguo.ni <neefrankie@gmail.com>
2020-01-22 15:30:25 +0800

    refactor: isolate CRUD for Gallery

--- repository/galleries.go
+++ repository/galleries.go
@@ -1,6 +1,11 @@
 package repository
 
-import "gitlab.com/ftchinese/content-api/models"
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/patrickmn/go-cache"
+	"gitlab.com/ftchinese/content-api/models"
+	"time"
+)
 
 type imageResult struct {
 	success []models.GalleryItem
@@ -12,7 +17,40 @@ type galleryResult struct {
 	err     error
 }
 
-func (env ContentEnv) RetrieveGalleryImages(id int64) ([]models.GalleryItem, error) {
+type GalleryEnv struct {
+	db    *sqlx.DB
+	cache *cache.Cache
+}
+
+func NewGalleryEnv(db *sqlx.DB) GalleryEnv {
+	return GalleryEnv{
+		db:    db,
+		cache: cache.New(30*time.Minute, 30*time.Minute),
+	}
+}
+
+func (env GalleryEnv) LoadGallery(id string) (models.Gallery, error) {
+	log := logger.WithField("trace", "GalleryEnv.LoadGallery")
+
+	if g, ok := env.getCachedGallery(id); ok {
+		log.Infof("Loaded gallery %s from cache", id)
+		return g, nil
+	}
+
+	g, err := env.RetrieveGallery(id)
+	if err != nil {
+		log.Error(err)
+		return models.Gallery{}, err
+	}
+
+	env.cacheGallery(g)
+
+	log.Infof("Loaded gallery %s from db", id)
+
+	return g, nil
+}
+
+func (env GalleryEnv) retrieveGalleryImages(id string) ([]models.GalleryItem, error) {
 	var items = make([]models.GalleryItem, 0)
 
 	if err := env.db.Select(&items, stmtGalleryImages, id); err != nil {
@@ -22,22 +60,22 @@ func (env ContentEnv) RetrieveGalleryImages(id int64) ([]models.GalleryItem, err
 	return items, nil
 }
 
-func (env ContentEnv) RetrieveGalleryBody(id int64) (models.Gallery, error) {
+func (env GalleryEnv) retrieveGalleryBody(id string) (models.Gallery, error) {
 	var data models.RawGallery
 
 	if err := env.db.Get(&data, stmtGallery, id); err != nil {
 		return models.Gallery{}, err
 	}
 
-	return models.NewGallery(&data), nil
+	return data.Build(), nil
 }
 
-func (env ContentEnv) RetrieveGallery(id int64) (models.Gallery, error) {
+func (env GalleryEnv) RetrieveGallery(id string) (models.Gallery, error) {
 	imageChan := make(chan imageResult)
 	bodyChan := make(chan galleryResult)
 
 	go func() {
-		images, err := env.RetrieveGalleryImages(id)
+		images, err := env.retrieveGalleryImages(id)
 		imageChan <- imageResult{
 			success: images,
 			err:     err,
@@ -45,7 +83,7 @@ func (env ContentEnv) RetrieveGallery(id int64) (models.Gallery, error) {
 	}()
 
 	go func() {
-		body, err := env.RetrieveGalleryBody(id)
+		body, err := env.retrieveGalleryBody(id)
 		bodyChan <- galleryResult{
 			success: body,
 			err:     err,
@@ -65,4 +103,23 @@ func (env ContentEnv) RetrieveGallery(id int64) (models.Gallery, error) {
 	gResult.success.Items = iResult.success
 
 	return gResult.success, nil
+}
+
+func (env GalleryEnv) cacheGallery(g models.Gallery) {
+	logger.WithField("trace", "GalleryEnv.cacheGallery").Infof("Caching new gallery %s", g.ID)
+
+	env.cache.Set(g.ID, g, cache.DefaultExpiration)
+}
+
+func (env GalleryEnv) getCachedGallery(id string) (models.Gallery, bool) {
+	x, found := env.cache.Get(id)
+	if !found {
+		return models.Gallery{}, false
+	}
+
+	if g, ok := x.(models.Gallery); ok {
+		return g, true
+	}
+
+	return models.Gallery{}, false
 }
