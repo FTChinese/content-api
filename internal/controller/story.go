25982ae7e5ae08202debffc7455693be125765d5
weiguo.ni <neefrankie@gmail.com>
2022-09-23 17:43:03 +0800

    replace view.JSON

--- internal/controller/story.go
+++ internal/controller/story.go
@@ -2,7 +2,6 @@ package controller
 
 import (
 	"github.com/FTChinese/go-rest/render"
-	"github.com/FTChinese/go-rest/view"
 	"github.com/jmoiron/sqlx"
 	"gitlab.com/ftchinese/content-api/internal/pkg"
 	"gitlab.com/ftchinese/content-api/internal/repository"
@@ -23,16 +22,16 @@ func NewStoryRouter(db *sqlx.DB, l *zap.Logger) StoryRouter {
 	}
 }
 
-func (router StoryRouter) Raw(w http.ResponseWriter, req *http.Request) {
+func (router StoryRouter) Story(w http.ResponseWriter, req *http.Request) {
 	id, err := xhttp.GetURLParam(req, "id").ToString()
 	if err != nil {
-		_ = view.JSON(w, view.NewBadRequest(err.Error()))
+		_ = render.New(w).BadRequest(err.Error())
 		return
 	}
 
 	story, err := router.repo.LoadRawStory(id)
 	if err != nil {
-		_ = view.JSON(w, view.NewDBFailure(err))
+		_ = render.New(w).DBError(err)
 		return
 	}
 
