dcc60173c0fb1bf8477bab6a96aacc762e942498
weiguo.ni <neefrankie@gmail.com>
2023-04-20 16:05:42 +0800

    xhttp pkg

---
+++ pkg/xhttp/user_ids.go
@@ -0,0 +1,31 @@
+package xhttp
+
+import (
+	"net/http"
+	"net/url"
+
+	"github.com/guregu/null"
+	"gitlab.com/ftchinese/content-api/pkg/ids"
+)
+
+func UserIDsFromHeader(h http.Header) ids.UserIDs {
+	ftcID := h.Get(XUserID)
+	unionID := h.Get(XUnionID)
+
+	return ids.UserIDs{
+		CompoundID: "",
+		FtcID:      null.NewString(ftcID, ftcID != ""),
+		UnionID:    null.NewString(unionID, unionID != ""),
+	}.MustNormalize()
+}
+
+func UserIDsFromQuery(v url.Values) ids.UserIDs {
+	ftcId := v.Get("ftc_id")
+	unionID := v.Get("union_id")
+
+	return ids.UserIDs{
+		CompoundID: "",
+		FtcID:      null.NewString(ftcId, ftcId != ""),
+		UnionID:    null.NewString(unionID, unionID != ""),
+	}.MustNormalize()
+}
