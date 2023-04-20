b9d8f97b1384c74070feaed4f1f74ce9c064a8a6
weiguo.ni <neefrankie@gmail.com>
2023-04-20 16:04:11 +0800

    generic paged list

--- internal/pkg/async_result.go
+++ internal/pkg/async_result.go
@@ -1,6 +1,14 @@
 package pkg
 
-type AsyncResult[T interface{}] struct {
+import gorest "github.com/FTChinese/go-rest"
+
+type AsyncResult[T any] struct {
 	Value T
 	Err   error
+}
+
+type PagedList[T any] struct {
+	Total int64 `json:"total" db:"row_count"`
+	gorest.Pagination
+	Data []T `json:"data"`
 }
