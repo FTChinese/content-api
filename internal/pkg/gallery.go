bf2ed63671ca3279b737a5160ee93f8bdc4a4612
weiguo.ni <neefrankie@gmail.com>
2023-04-20 10:00:53 +0800

    add sql stmt

--- internal/pkg/gallery.go
+++ internal/pkg/gallery.go
@@ -13,6 +13,13 @@ type GalleryItem struct {
 	Caption  string `json:"caption" db:"caption"`
 }
 
+const StmtGalleryItem = `
+SELECT TRIM(BOTH FROM pic_url) AS image_url,
+    TRIM(BOTH FROM pbody) AS caption
+FROM cmstmp01.photonews_picture
+WHERE photonewsid = ?
+ORDER BY orders`
+
 // Gallery is a piece of photo news
 type Gallery struct {
 	Teaser
@@ -33,3 +40,15 @@ func (r RawGallery) Build() Gallery {
 		Items:  []GalleryItem{},
 	}
 }
+
+const StmtGallery = `
+SELECT photonewsid AS id,
+    add_times AS created_at,
+    accessright AS access_right,
+    cn_title AS title_cn,
+    TRIM(BOTH FROM shortlead) AS long_lead_cn,
+    leadbody AS body,
+    cover AS cover_url
+FROM cmstmp01.photonews
+WHERE photonewsid = ?
+LIMIT 1`
