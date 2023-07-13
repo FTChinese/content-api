da765a0722057b88f576efc89956ad02f2bac508
weiguo.ni <neefrankie@gmail.com>
2023-07-13 11:26:26 +0800

    refactor: sql for interactive

--- internal/pkg/raw_interactive_stmt.go
+++ internal/pkg/raw_interactive_stmt.go
@@ -8,7 +8,7 @@ FROM cmstmp01.interactive_story AS story
         ON picture.id = a_p.picture_id
 `
 
-const StmtInteractiveTeaser = `
+const stmtSelectInteractiveBase = `
 SELECT story.id,
     story.fileupdatetime AS created_at,
     story.last_publish_time AS updated_at,
@@ -17,24 +17,18 @@ SELECT story.id,
     story.cshortleadbody AS short_lead_cn,
     story.tag,
     picture.piclink AS cover_url
-` + stmtInteractiveFrom + `
+`
+
+const StmtInteractiveTeaser = stmtSelectInteractiveBase + stmtInteractiveFrom + `
 WHERE find_in_set(?, story.tag)
     AND story.publish_status = 'queue'
 ORDER BY story.fileupdatetime DESC
 LIMIT ? OFFSET ?`
 
-const StmtInteractiveContent = `
-SELECT story.id AS id,
-    story.fileupdatetime AS created_utc,
-    story.last_publish_time AS updated_utc,
-    story.cheadline AS title_cn,
+const StmtInteractiveContent = stmtSelectInteractiveBase + `,
     story.eheadline AS title_en,
-    story.clongleadbody AS long_lead_cn,
     story.elongleadbody AS long_lead_en,
-    story.cshortleadbody AS short_lead_cn,
     CONCAT(story.cbyline_description, story.cauthor) AS byline_cn,
-    story.tag AS tag,
-    picture.piclink AS cover_url,
     story.cbody AS body_cn,
     story.ebody AS body_en
 ` + stmtInteractiveFrom + `
